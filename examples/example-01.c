21614d3a85eafc285249d05b6ace6eecef05fdea
Jorge Niedbalski R <niedbalski@gmail.com>
2013-01-22 18:14:00 -0300

    Modified makefile, added the list.h instead of use it on lua.h

--- examples/example-01.c
+++ examples/example-01.c
@@ -1,27 +1,25 @@
 #include "backtest.h"
 
-int main(void) {
+
+int main(void) 
+{
     lua_context *ctx;
     lua_arg argument;
-
     int ret;
 
-    ctx = backtest_lua_init("./plugins/moving.luas");
+    ctx = backtest_lua_init("./plugins/moving.lua");
 
     if(ctx == NULL)
-        goto error;
+        return EXIT_FAILURE;
 
     argument.value.number = 100.0;
     argument.type = NUMBER;
 
-    if( (ret = ctx->hook(ctx, "on_tick", 1, argument)) != 0)
-        goto error;
-
-error:
-    if(ctx != NULL) {
+    if((ret = ctx->hook(ctx, "on_tick", 1, argument)) != 0) {
         backtest_lua_destroy(ctx);
         return EXIT_FAILURE;
     }
 
     return EXIT_SUCCESS;
+
 }
