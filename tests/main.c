c606a0e40bb9ad4dec6e64d9af0f6336db1e6ed2
Jorge Niedbalski R <niedbalski@gmail.com>
2013-01-25 19:41:21 -0300

    Return number of failures > 0 for travis

--- tests/main.c
+++ tests/main.c
@@ -15,7 +15,7 @@ int finish_suite(void) {
 int main() {
 
     CU_pSuite backtest_suite = NULL;
-
+    unsigned int num_failures;
 
     if (CUE_SUCCESS != CU_initialize_registry())
         return CU_get_error();
@@ -37,7 +37,11 @@ int main() {
 
     CU_basic_set_mode(CU_BRM_VERBOSE);
     CU_basic_run_tests();
+   
+    num_failures = CU_get_number_of_failures();
     CU_cleanup_registry();
 
-    return CU_get_error();
+    
+
+    return num_failures;
 }
